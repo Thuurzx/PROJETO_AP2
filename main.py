4cfdeadb1452a44463ede7822e1764b610b7f85b
Thuurzx <masterarthurw@gmail.com>
2026-05-24 14:07:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -601,6 +601,11 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+[data-testid="stSidebar"][aria-expanded="false"] {
+    display: block !important;
+    width: 21rem !important;
+    transform: none !important;
+}
  
 # ─────────────────────────────────────────────
 # CSS PREMIUM
